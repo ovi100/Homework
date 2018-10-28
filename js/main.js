2a2b1ca1bcd81b4eaf0019844f1116f88461eef3
ovi100 <ovi0167@hotmail.com>
2018-10-28 22:24:06 +0600

    Theme Color Change and Gallery Slideshow added

--- js/main.js
+++ js/main.js
@@ -8,51 +8,15 @@
 
 $(document).ready(function(){
 
-	$(window).scroll(function(){
-	    if ($(this).scrollTop()>150){
-	      // $('.menu').addClass('fixed-menu');
-	      
-	    } 
-	    else {
-	      // $('.menu').removeClass('fixed-menu');
-	    }
-  	});
-
-  	// if ($(window).width()<768){
-   //    $('.col-logo').addClass('order-12');
-   //    $('.col-icon').addClass('order-1');
-      
-   //  } 
-   //  else {
-   //    $('.col-logo').removeClass('order-12');
-   //    $('.col-icon').removeClass('order-1');
-   //  }
-
-
-   // Home Slider
-
- //    $('#main-slider').owlCarousel({
- //    	autoplay:true,
- //    	autoplayHoverPause:true,
- //    	dots:false,
- //    	items:2,
-	//     loop:true,
-	//     margin:0,
-	//     nav:true,
-	//     navText:['<i class="icofont">rounded_left</i>','<i class="icofont">rounded_right</i>'],
-	//     responsive:{
-	//         0:{
-	//             items:1
-	//         },
-	//         600:{
-	//             items:1
-	//         },
-	//         767:{
-	//             items:2
-	//         }
-	//     },
-	//     slideBy:2
-	// })
+	$('[data-fancybox]').fancybox({
+	  protect: true,
+	  buttons : [
+	    'zoom',
+	    'thumbs',
+	    'close',
+	    'slideShow'
+	  ]
+	});
 
 
 });
